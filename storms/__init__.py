9936ce02d974b53cf46e6854d2d022f8b75e80ae
ckaros <ckaros@outlook.com>
2023-11-30 13:06:45 -0500

    set release version

--- storms/__init__.py
+++ storms/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for storms."""
 
 __author__ = """Constantine Karos"""
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 
 from storms.precip import Raingage
 from storms.precip import Network
